b24d3aa496396b5a917aebfa44530f057fc2b24e
Lauri Vasko <jauv5oe7ST##>
2017-11-14 14:03:59 +0200

    initial commit for config-repository

---
+++ config_server/src/main/java/com/github/lvasko/config/ConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package com.github.lvasko.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by 38102090301 on 14.11.2017.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
